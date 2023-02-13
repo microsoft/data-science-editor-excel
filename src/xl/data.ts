f53a1264f6960a8aab236cc45953b27c193e09e9
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-02-13 21:54:53 +0000

    prettier flags

--- src/xl/data.ts
+++ src/xl/data.ts
@@ -1,63 +1,66 @@
-export type CellData = { [header: string]: string | number | boolean }[];
+export type CellData = { [header: string]: string | number | boolean }[]
 
 export async function getSelectedData(): Promise<CellData> {
-    return await Excel.run(async context => {
-        const range = context.workbook.getSelectedRange();
+    return await Excel.run(async (context) => {
+        const range = context.workbook.getSelectedRange()
 
-        return await getDataForRange(context, range);
-    });
+        return await getDataForRange(context, range)
+    })
 }
 
 export async function getDataForAddress(address: string): Promise<CellData> {
-    return await Excel.run(async context => {
-        const sheet = context.workbook.worksheets.getActiveWorksheet();
-        const range = sheet.getRange(address);
+    return await Excel.run(async (context) => {
+        const sheet = context.workbook.worksheets.getActiveWorksheet()
+        const range = sheet.getRange(address)
 
-        return await getDataForRange(context, range);
-    });
+        return await getDataForRange(context, range)
+    })
 }
 
 export async function getDataForTable(tableName: string): Promise<CellData> {
-    return await Excel.run(async context => {
-        const sheet = context.workbook.worksheets.getActiveWorksheet();
-        const table = sheet.tables.getItemOrNullObject(tableName);
+    return await Excel.run(async (context) => {
+        const sheet = context.workbook.worksheets.getActiveWorksheet()
+        const table = sheet.tables.getItemOrNullObject(tableName)
 
         try {
-            await context.sync();
+            await context.sync()
 
             if (!table.isNullObject) {
-                const r = await getDataForRange(context, table.getRange());
+                const r = await getDataForRange(context, table.getRange())
                 return r
             }
         } catch (e) {
-            console.error(e);
+            console.error(e)
         }
 
-        return [];
-    });
+        return []
+    })
 }
 
-async function getDataForRange(context: Excel.RequestContext, range: Excel.Range): Promise<CellData> {
-    range.load("values");
+async function getDataForRange(
+    context: Excel.RequestContext,
+    range: Excel.Range
+): Promise<CellData> {
+    range.load("values")
 
-    await context.sync();
+    await context.sync()
 
-    const values = range.values as (string | number | boolean)[][];
-    const headers = values[0].map(value => value.toString());
+    const values = range.values as (string | number | boolean)[][]
+    const headers = values[0].map((value) => value.toString())
 
     // Remove the header row, filter out empty rows, and map each cell in each
     // row to its header.
     const r = values
         .slice(1)
-        .filter(cells => cells.some(cell => cell !== ""))
-        .map(cells => {
-            const row = {};
+        .filter((cells) => cells.some((cell) => cell !== ""))
+        .map((cells) => {
+            const row = {}
             headers.forEach((header, index) => {
-                row[header] = cells[index];
-            });
+                row[header] = cells[index]
+            })
 
-            return row;
-        });
+            return row
+        })
     console.log(`data.range`, { r, range })
     return r
 }
